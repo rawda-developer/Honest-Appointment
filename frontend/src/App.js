38b5647c56c288e1de270aa00cb40ab706df31aa
Rawda Yasser <rawda.developer@gmail.com>
2022-05-26 19:48:11 +0200

    First commit: add front end with chakra ui template

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
